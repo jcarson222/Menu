5d7543fff006d93935cb77963767cfa436ebe5cd
Jesse Carson <jmcarson222@gmail.com>
2023-01-12 11:13:21 -0600

    Filter buttons
    Menu items will display based on the button clicked with the corresponding data-id

--- app.js
+++ app.js
@@ -74,23 +74,51 @@ const menu = [
 ];
 
 const sectionCenter = document.querySelector(".section-center");
+const filterBtns = document.querySelectorAll(".filter-btn");
+
+//load items
 
 window.addEventListener("DOMContentLoaded", () => {
-  let displayMenu = menu.map((item) => {
+  displayMenuItems(menu);
+});
+
+//filter items
+
+filterBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const category = e.currentTarget.dataset.id;
+
+    const menuCategory = menu.filter((menuItem) => {
+      if(menuItem.category === category) {
+        return menuItem;
+      }
+    });
+
+    if(category === "all") {
+      displayMenuItems(menu);
+    } else {
+      displayMenuItems(menuCategory);
+    }
+  });
+});
+
+const displayMenuItems = (menuItems) => {
+  let displayMenu = menuItems.map((item) => {
     
     return `<article class="menu-item">
-    <img src=${item.img} class="photo" alt="${item.title}" />
-    <div class="item-info">
-      <header>
-        <h4>${item.title}</h4>
-        <h4 class="price">$${item.price}</h4>
-      </header>
-      <p class="item-text">${item.desc}</p>
-    </div>
-  </article>`;
-  })
+              <img src=${item.img} class="photo" alt="${item.title}" />
+              <div class="item-info">
+               <header>
+                <h4>${item.title}</h4>
+                <h4 class="price">$${item.price}</h4>
+               </header>
+               <p class="item-text">
+                ${item.desc}
+               </p>
+              </div>
+            </article>`;
+  });
 
   displayMenu = displayMenu.join("");
   sectionCenter.innerHTML = displayMenu;
-});
-
+};
